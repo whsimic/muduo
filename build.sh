f08fd321139bb8393b0583303717140140375935
whsimic <whsimic@gmail.com>
2021-05-01 10:57:36 +0800

    change build option

--- build.sh
+++ build.sh
@@ -3,18 +3,19 @@
 set -x
 
 SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
-BUILD_TYPE=${BUILD_TYPE:-release}
-INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
-CXX=${CXX:-g++}
+BUILD_DIR=${BUILD_DIR:-$SOURCE_DIR/../muduo_build}
+BUILD_TYPE=${BUILD_TYPE:-debug}
+INSTALL_DIR=${INSTALL_DIR:-$SOURCE_DIR/../muduo_install}
+#CXX=${CXX:-g++}
 
-ln -sf $BUILD_DIR/$BUILD_TYPE-cpp11/compile_commands.json
+ln -sf $BUILD_DIR/$BUILD_TYPE/compile_commands.json
 
-mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
-  && cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
+mkdir -p $BUILD_DIR/$BUILD_TYPE \
+  && mkdir -p $INSTALL_DIR/$BUILD_TYPE \
+  && cd $BUILD_DIR/$BUILD_TYPE \
   && cmake \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-           -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+           -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR/$BUILD_TYPE \
            -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
            $SOURCE_DIR \
   && make $*
